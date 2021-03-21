84e876ad612a7c3137f3ef6cfef1678c1c98a543
Dan Moneypenny <catullus1973@gmail.com>
2021-03-21 03:15:41 -0700

    update

--- src/index.js
+++ src/index.js
@@ -28,7 +28,7 @@ const apiRouter = express.Router();
         defaultSrc: ["'self'"], 
         scriptSrc: ["'self'"],
         styleSrc: ["'self'", 'https://fonts.googleapis.com', "https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css", "https://cdn.auth0.com/js/auth0-samples-theme/1.0/css/auth0-theme.min.css", "'unsafe-inline'"],
-        imgSrc: ["'self'", 'data:'],
+        imgSrc: ["'self'", 'data:', "https://s.gravatar.com"],
         connectSrc: ["'self'", 'https://dpmsandbox.us.auth0.com/oauth/token'],
         fontSrc: ["'self'", 'https://fonts.gstatic.com'],
         objectSrc: ["'self'"],
