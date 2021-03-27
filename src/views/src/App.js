956ed8e6ed52c27fd6e9da3401d9045b20d7c87f
Dan Moneypenny <catullus1973@gmail.com>
2021-03-26 22:26:28 -0700

    add protected route

--- src/views/src/App.js
+++ src/views/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { ProtectedRoute } from './auth';
+import { useAuth0 } from '@auth0/auth0-react';
 import AppLayout from './components/layout';
 import { Landing as ProjectsLanding } from './pages/Projects';
 import { Landing as HomeLanding } from './pages/Home';
@@ -7,17 +9,24 @@ import NotFound from './pages/NotFound';
 
 function App() {  
 
+  const { isLoading } = useAuth0();
+
+  if(isLoading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     
-      <AppLayout>
-        <Switch>
-          <Route path="/" exact component={HomeLanding}/>
-          <Route path="/projects" component={ProjectsLanding}/>
-          <Route path="*" component={NotFound}/>
-        </Switch>
-      </AppLayout>      
+    <AppLayout>
+      <Switch>
+        <Route path="/" exact component={HomeLanding}/>
+        <ProtectedRoute path="/projects" component={ProjectsLanding}/>
+        <Route path="*" component={NotFound}/>
+      </Switch>
+    </AppLayout>      
   
   );
+
 }
 
 export default App;
--- src/views/src/components/layout/Header.js
+++ src/views/src/components/layout/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { AuthenticationButton } from './../../auth';
 
 const Header = ({ children }) => {
   return ( 
     <header>
       <h1>portfolio application</h1>
+      <p><AuthenticationButton/></p>
       { children }
     </header>
    );
