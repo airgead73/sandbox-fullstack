bc6e70005b26c6bec415eca8d277f0ef3252a78a
Dan Moneypenny <catullus1973@gmail.com>
2021-03-27 03:48:10 -0700

    use fetch hook

--- src/views/src/pages/Projects/List.js
+++ src/views/src/pages/Projects/List.js
@@ -1,63 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import { ListItem } from './index';
 import projects from './../../data/projects';
-import { useAuth0 } from '@auth0/auth0-react';
+import useFetch from '../../fetch/useFetch';
 
 const List = () => {
 
-  const [projects, setProjects] = useState(null);
-  const [error, setError] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-
-  const { getAccessTokenSilently } = useAuth0();
-
-  const callApi = async () => {
-    try {
-
-      console.log('api called')
-      const token = await getAccessTokenSilently();
-
-      const response = await fetch(`${process.env.REACT_APP_SERVER_URL_DEV}/api/projects`, {
-        headers: {
-          Authorization: `Bearer ${token}`
-        }        
-      });
-
-      const responseData = await response.json();
-
-      console.log(responseData);
-
-      const { success, message, results } = responseData;
-
-      if(!success) {
-        console.log(message);
-        setError(message);
-        setIsLoading(false);
-      }
-
-      setProjects(results);
-      setIsLoading(false);
-
-    } catch(err) {
-      console.log(err)
-    }
-  }
-
-  useEffect(() => {
-
-    callApi();
-
-  },[]);
-
-  if(isLoading) {
-    return <div>loading...</div>
-  }
+  const { data: projects, isLoading, error } = useFetch('/api/projects');
 
   return (
     
     <section>
       <h3>projects list</h3>
       {error && <p>{error}</p>}
+      {isLoading && <p>Loading...</p>}
       {projects && <ul>
         {projects.map((project) => (
           <ListItem key={project._id} item={project}/>
