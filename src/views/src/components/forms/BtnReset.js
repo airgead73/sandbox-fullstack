7e16e5b0c3e439aebf2c3fe5bdd3d9e91a11cf33
Dan Moneypenny <catullus1973@gmail.com>
2021-03-29 23:18:11 -0700

    clear out files

--- src/views/src/components/forms/BtnReset.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const BtnReset = ({title}) => {
-  return ( 
-    <button type="reset">{title}</button>
-   );
-}
- 
-export default BtnReset;
--- src/views/src/components/forms/BtnSubmit.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const BtnSubmit = ({title}) => {
-  return ( 
-    <button type="submit">{title}</button>
-   );
-}
- 
-export default BtnSubmit;
